ec7938158fc95bac8e5e12a48644fa626949de61
Adınız <siz@e-posta.com>
2021-10-30 15:19:42 +0200

    AC-3 completed for Sudoku 1-2-5

--- sudoku-csp/src/Game.java
+++ sudoku-csp/src/Game.java
@@ -1,9 +1,11 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class Game {
-  private Sudoku sudoku;
 
+
+public class Game {
+  private Sudoku sudoku; 
+  
   Game(Sudoku sudoku) {
     this.sudoku = sudoku;
   }
@@ -19,10 +21,8 @@ public class Game {
    */
   public boolean solve() {
     // TODO: implement AC-3
-
     // For every field initialize its arcs (if it is a constant value, i.e. known then its arc list is empty)
-    //Then add these arcs to the queue.
-    List<Arc> arcs = new ArrayList<>(); // arc list of the game
+    List<Arc> arcs =new ArrayList<>();// arc list of the game
     for (Field[] row : sudoku.getBoard())
     {
       for (Field field :row)
@@ -31,18 +31,22 @@ public class Game {
         arcs.addAll(field.getArcs());
       }
     }
-    List<Arc> listOfArcs = arcs; //agenda of arcs
-    
-    //while queue is not empty
 
-    while(!listOfArcs.isEmpty())
+    //Then add these arcs to the agenda.
+
+    List<Arc> agenda = new ArrayList<>();//agenda of arcs
+    agenda.addAll(arcs);
+    
+    
+    //while agenda is not empty
+    int ct =0;
+    while(!agenda.isEmpty())
     {
       //take the first element first arc
-      Arc arc = listOfArcs.get(0);
-      listOfArcs.remove(0);
-      Integer arcLeftDomainSize = arc.getLeftHandSide().getDomainSize();
-
-      //revise the arc \\removeFromDomain 
+      Arc arc = agenda.get(0);
+      agenda.remove(0);
+      int arcLeftDomainSize = arc.getLeftHandSide().getDomainSize();
+      //revise the arc  
       revise(arc);
       // if the new size of domain is zero return false 
       if(arc.getLeftHandSide().getDomainSize() == 0)
@@ -51,14 +55,16 @@ public class Game {
       }
       
       // if the domain size has changed
-      if(arcLeftDomainSize - arc.getLeftHandSide().getDomainSize() !=0 )
+      if(arcLeftDomainSize != arc.getLeftHandSide().getDomainSize() )
       {
         //for all arcs contain the left hand side as the right hand side add to the list
-        listOfArcs.addAll(findRightHandSides(arc.getLeftHandSide(),arcs , listOfArcs));
+        agenda.addAll(findRightHandSides(arc.getLeftHandSide(),arcs , agenda));
+        ct++; 
+       
       }
-
+        
     }
-    
+    System.out.println(ct);
     return true;
   }
 
@@ -76,7 +82,7 @@ public class Game {
   {
     Field leftHand = arc.getLeftHandSide();
     Field rightHand = arc.getRightHandSide();
-    List<Integer> toRemove = new ArrayList();
+    List<Integer> toRemove = new ArrayList<>();
     for (int valueLeft : (leftHand.getDomain())) //for all values in the domain of left hand side
     {
       for(int valueRight : rightHand.getDomain()) //there exists a value meets the arc in the domain of right hand side
@@ -87,7 +93,8 @@ public class Game {
         }
         // if there is left no value that meets the arc in the domain of right hand side
         // which means the value is the last element of the domain and it does not meet the arc
-        else if (rightHand.getDomain().indexOf(valueRight) == rightHand.getDomainSize()-1)
+        else if (rightHand.getDomain().indexOf(valueRight)== rightHand.getDomainSize()-1)
+        
         {
           toRemove.add(valueLeft); //remove that value from the domain of left hand side.
         }
@@ -105,7 +112,7 @@ public class Game {
    * @param arcList  the list of arcs to be looked at
    * @return a list of arcs where the right hand side is equal to leftHandSide
    */
-  public List<Arc> findRightHandSides(Field leftHandSide, List<Arc> arcList, List<Arc> arcs)
+  public List<Arc> findRightHandSides(Field leftHandSide, List<Arc> arcList, List<Arc> agenda)
   {
     List<Arc> arcsFound = new ArrayList<>();
 
@@ -113,7 +120,7 @@ public class Game {
     {
       if(leftHandSide == arc.getRightHandSide())  // if the lefthandside is same as the arc's right hand side
       {
-          if(!arcs.contains(arc)) // and if that arc is not in the agenda
+          if(!agenda.contains(arc)) // and if that arc is not in the agenda
           {
             arcsFound.add(arc); //add that arc to the agenda
           }
